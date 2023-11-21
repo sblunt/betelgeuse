31f8465921cf9abd0f134d6735a513207cd4d759
Sarah Blunt <sarah.blunt.3@gmail.com>
2023-11-21 13:18:06 -0500

    first attempt at running the fit!

--- data/make_orbitize_input.py
+++ data/make_orbitize_input.py
@@ -8,7 +8,17 @@ import matplotlib.pyplot as plt
 
 """
 Convenience script to convert data from download format to orbitize format.
+
+Note: Hipparcos measurements are given in ICRS frame. Both tables are also
+given in ICRS frame, at epoch J2000. 
+
+
 """
+
+# TODO: convert measurements to offsets from measured pos at 1991.25 (and propagate errors)
+# note that the measurements from both data tables here are reported in J2000, so need to convert them to J1991.25
+
+# TODO: first test that I can recover Harper+ astrometric solution: https://iopscience.iop.org/article/10.1088/0004-6256/135/4/1430
 
 table1 = pd.read_csv(
     "radio_pos_table1.csv",
@@ -58,7 +68,7 @@ n_obs = len(df)
 df_orbitize = DataFrame(
     Time(df["date"].astype("float"), format="decimalyear").mjd, columns=["epoch"]
 )
-df_orbitize["object"] = np.ones_like(df_orbitize["epoch"]).astype(int)
+df_orbitize["object"] = np.zeros_like(df_orbitize["epoch"]).astype(int)
 df_orbitize["ra"] = np.ones_like(df_orbitize["epoch"])
 df_orbitize["dec"] = np.ones_like(df_orbitize["epoch"])
 
@@ -79,11 +89,17 @@ for i in range(n_obs):
     df_orbitize["ra"][i] = float(ra)  # [deg]
     df_orbitize["dec"][i] = float(dec)  # [deg]
 
-# take difference between epoch 0 and convert to mas
-df_orbitize["ra"] = (df_orbitize["ra"] - df_orbitize["ra"][0]) * u.deg.to(
+# take difference between reported Hipparcos position and convert to mas
+
+# read the Hipparcos best-fit solution from the IAD file
+astrometric_solution = pd.read_csv("H027989.d", skiprows=9, sep="\s+", nrows=1)
+hipparcos_alpha0 = astrometric_solution["RAdeg"].values[0]  # [deg]
+hipparcos_delta0 = astrometric_solution["DEdeg"].values[0]  # [deg]
+
+df_orbitize["ra"] = (df_orbitize["ra"] - hipparcos_alpha0) * u.deg.to(
     u.mas, equivalencies=u.dimensionless_angles()
 )
-df_orbitize["dec"] = (df_orbitize["dec"] - df_orbitize["dec"][0]) * u.deg.to(
+df_orbitize["dec"] = (df_orbitize["dec"] - hipparcos_delta0) * u.deg.to(
     u.mas, equivalencies=u.dimensionless_angles()
 )
 df_orbitize["dec_err"] = df["dec_err"].astype(float)
--- fit_orbit.py
+++ fit_orbit.py
@@ -1,6 +1,12 @@
 import os.path
-import orbitize
-from orbitize import read_input, hipparcos, gaia, system, priors, sampler
+from orbitize import read_input, hipparcos, system, priors, sampler
+
+
+"""
+Set some options for the fit
+"""
+
+fit_planet = False  # if True, fit for planet parameters
 
 input_file = os.path.join("data/data.csv")
 data_table = read_input.read_file(input_file)
@@ -8,6 +14,8 @@ data_table = read_input.read_file(input_file)
 num_secondary_bodies = 1  # number of planets/companions orbiting your primary
 hip_num = "027989"  # Betelgeuse
 IAD_file = "data/H{}.d".format(hip_num)
+
+# the angular size of Beetle is 55 mas, and the Hipparcos jitter is 0.15 mas, for reference
 
 beetle_Hip = hipparcos.HipparcosLogProb(IAD_file, hip_num, num_secondary_bodies)
 
@@ -32,6 +40,11 @@ beetle_system = system.System(
     fitting_basis="Period",
 )
 
+# make sure orbitize! knows to fit for the proper motion and parallax
+assert beetle_system.pm_plx_predictor is not None
+
+# make sure orbitize! correctly figures out which epochs are absolute astrometry
+assert len(beetle_system.stellar_astrom_epochs) == 18
 """
 Change priors
 """
@@ -40,30 +53,54 @@ Change priors
 plx_index = beetle_system.param_idx["plx"]
 beetle_system.sys_priors[plx_index] = priors.UniformPrior(7.6 - 5.0, 7.6 + 5.0)
 
-# set uniform primary mass prior
+
 m0_index = beetle_system.param_idx["m0"]
-beetle_system.sys_priors[m0_index] = priors.UniformPrior(
-    15, 20
-)  # big range, lots of unc for Betelgeuse
-
-# set log-uniform secondary mass prior
 m1_index = beetle_system.param_idx["m1"]
-beetle_system.sys_priors[m1_index] = priors.LogUniformPrior(0.1, 10)
-
-# set period prior between 3 and 10 years
 p1_index = beetle_system.param_idx["per1"]
-beetle_system.sys_priors[p1_index] = priors.UniformPrior(3, 10)
+e1_index = beetle_system.param_idx["ecc1"]
+pan1_index = beetle_system.param_idx["pan1"]
+aop1_index = beetle_system.param_idx["aop1"]
+inc1_index = beetle_system.param_idx["inc1"]
+tau1_index = beetle_system.param_idx["tau1"]
 
+
+if fit_planet:
+    # set uniform primary mass prior
+    beetle_system.sys_priors[m0_index] = priors.UniformPrior(
+        15, 20
+    )  # big range, lots of unc for Betelgeuse
+
+    # set log-uniform secondary mass prior
+    beetle_system.sys_priors[m1_index] = priors.LogUniformPrior(0.1, 10)
+
+    # set period prior between 3 and 10 years
+    beetle_system.sys_priors[p1_index] = priors.UniformPrior(3, 10)
+
+else:
+    # set the planet parameters so that the planet signal is 0 (i.e. only model the astrometry)
+    beetle_system.sys_priors[m0_index] = 10
+    beetle_system.sys_priors[m1_index] = 0  # test particle secondary
+    beetle_system.sys_priors[p1_index] = 1
+    beetle_system.sys_priors[e1_index] = 0
+    beetle_system.sys_priors[pan1_index] = 0
+    beetle_system.sys_priors[aop1_index] = 0
+    beetle_system.sys_priors[inc1_index] = 0
+    beetle_system.sys_priors[tau1_index] = 0
+
+# print out the priors to make sure everything looks fine
+print(list(zip(beetle_system.labels, beetle_system.sys_priors)))
 
 """
 Run MCMC
 """
-num_threads = 50
-num_temps = 20
-num_walkers = 1000
-num_steps = 10000000  # n_walkers x n_steps_per_walker
-burn_steps = 10000
-thin = 100
+num_threads = 10  # 50
+num_temps = 5  # 20
+num_walkers = 10  # 1000
+n_steps_per_walker = 1_000  # 10_000
+num_steps = num_walkers * n_steps_per_walker
+burn_steps = 1_000  # 10_000
+thin = 1  # 100
+
 
 beetle_sampler = sampler.MCMC(
     beetle_system,
@@ -71,4 +108,4 @@ beetle_sampler = sampler.MCMC(
     num_temps=num_temps,
     num_walkers=num_walkers,
 )
-# beetle_sampler.run_sampler(num_steps, burn_steps=burn_steps, thin=thin)
+beetle_sampler.run_sampler(num_steps, burn_steps=burn_steps, thin=thin)
